7db4df670a05e1de34ec607e9ca66d9ca6edfaec
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-04-29 20:39:13 +0300

    Task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -5,3 +5,21 @@
 // 3. Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // 4. Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // 5. Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', hundleSubmit);
+
+function hundleSubmit(event) {
+    event.preventDefault();
+
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert("Please, fill in all the fields!")
+    }
+
+    const data = { email: email.value, password: password.value };
+    console.log(data);
+    event.currentTarget.reset();
+};
