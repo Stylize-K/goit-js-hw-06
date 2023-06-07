44006af64f6d537b9936e40c03bb22519f034516
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-06-07 04:37:29 +0300

    Fix bug

--- js/task-10.js
+++ js/task-10.js
@@ -9,54 +9,51 @@
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
 
-const InputEl = document.querySelector('#controls input');
-const btnCreate = document.querySelector('[data-create]');
-const btnDestroy = document.querySelector('[data-destroy]');
-const boxesEl = document.querySelector('#boxes');
+const InputEl = document.querySelector("#controls input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
 
 btnCreate.addEventListener("click", createBoxes);
-btnDestroy.addEventListener('click', destroyBoxes);
-
+btnDestroy.addEventListener("click", destroyBoxes);
 
 function destroyBoxes() {
-  boxesEl.innerHTML = '';
+	boxesEl.innerHTML = "";
+	InputEl.value = "";
 }
 
-// Не зміг реалізувати функцію createBoxes(amount), яка приймає число amount. Я оголошую змінну amount в середині функції. 
+// Не зміг реалізувати функцію createBoxes(amount), яка приймає число amount. Я оголошую змінну amount в середині функції.
 
 function createBoxes() {
+	const amount = InputEl.value;
+	let side = 30;
+	let markup = "";
 
-  const amount = InputEl.value;
-  let side = 30;
-  let markup = '';
-  
-  for (let i = 1; i <= amount; i += 1) {
-    if (i === 1) {
-      const box = `<div style="height: ${side}px; width: ${side}px; background-color: ${getRandomHexColor()};"></div>`;
-      markup += box;
-    } else {
-        side += 10;
-        const box = `<div style="height: ${side}px; width: ${side}px; background-color: ${getRandomHexColor()};"></div>`;
-        markup += box;
-    }
-  }
-  boxesEl.insertAdjacentHTML("afterbegin", markup);
+	for (let i = 1; i <= amount; i += 1) {
+		if (i === 1) {
+			const box = `<div style="height: ${side}px; width: ${side}px; background-color: ${getRandomHexColor()};"></div>`;
+			markup += box;
+		} else {
+			side += 10;
+			const box = `<div style="height: ${side}px; width: ${side}px; background-color: ${getRandomHexColor()};"></div>`;
+			markup += box;
+		}
+	}
+	boxesEl.insertAdjacentHTML("afterbegin", markup);
 }
 
-
-// !!!!! - ДРУГИЙ ВАРІАНТ ФУНКЦІЇ createBoxes(), АЛЕ ТУТ НА КАОЖНІЙ ІТЕРАЦІЇ ВІДБУВАЄТЬСЯ ДОДАВАННЯ ПОТОЧНОГО ЕЛЕМЕНТА В DOM, ЩО НЕ Э ХОРОШОЮ ПРАКТИКОЮ З ПРИВОДУ ОПТИМіЗАЦІЇ - !!!!! 
-
+// !!!!! - ДРУГИЙ ВАРІАНТ ФУНКЦІЇ createBoxes(), АЛЕ ТУТ НА КАОЖНІЙ ІТЕРАЦІЇ ВІДБУВАЄТЬСЯ ДОДАВАННЯ ПОТОЧНОГО ЕЛЕМЕНТА В DOM, ЩО НЕ Э ХОРОШОЮ ПРАКТИКОЮ З ПРИВОДУ ОПТИМіЗАЦІЇ - !!!!!
 
 // function createBoxes() {
-  
+
 //   let amount = InputEl.value;
 //   let side = 30;
-  
+
 //   for (let i = 1; i <= amount; i += 1) {
 //     if (i === 1) {
 //       const box = document.createElement("div");
